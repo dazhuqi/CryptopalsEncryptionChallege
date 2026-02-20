e0a8fd2f43a7f385b9e07809fa6ae55ff1f2bfc6
dazhuqi <1260930860@qq.com>
2026-02-20 19:38:22 +0800

    feat: upload the answer of challenge 13

--- Set2_Block_Crypto/c13_ecb_cut_and_paste.py
+++ Set2_Block_Crypto/c13_ecb_cut_and_paste.py
@@ -34,3 +34,43 @@ Using only the user input to profile_for() (as an oracle to generate "valid" cip
 
 """
 
+import os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+# transfer "foo=bar&baz=qux&zap=zazzle" to dict
+def parse_kv(qs):
+    return {k: v for k, v in [pair.split('=') for pair in qs.split('&')]}
+
+# produce profile string and filter meta byte
+def profile_for(email):
+    email = email.replace('&', '').replace('=', '') # prevent injection
+    return f"email={email}&uid=10&role=user"
+
+# AES_ECB encrypt and decrypt tool
+KEY = os.urandom(16) # produce 16 bytes cipher
+
+def encrypt_profile(email):
+    plaintext = profile_for(email).encode()
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    return cipher.encrypt(pad(plaintext, 16))
+
+def decrypt_and_parse(ciphertext):
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    plaintext = unpad(cipher.decrypt(ciphertext), 16).decode()
+    return parse_kv(plaintext)
+
+pad_admin = "admin" + chr(11) * 11 # construct the block
+ct_admin_block = encrypt_profile("A" * 10 + pad_admin) # email prefix 10 bytes + admin block
+admin_part = ct_admin_block[16:32] # 2nd block
+
+email_prefix = "hacker@me.com"
+ct_main = encrypt_profile(email_prefix)
+main_part = ct_main[0:32]
+
+payload = main_part + admin_part
+
+# validation
+result = decrypt_and_parse(payload)
+print(f"[+] The object after encrypting: {result}")
+print(f"[+] Congratulation! Role is: {result['role']}")
