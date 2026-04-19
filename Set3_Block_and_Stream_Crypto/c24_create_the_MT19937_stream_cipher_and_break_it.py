3df815913f139abedaff49bcf29d9c1c7a1639cb
dazhuqi <1260930860@qq.com>
2026-04-19 11:19:09 +0800

    feat: upload the answer

--- Set3_Block_and_Stream_Crypto/c24_create_the_MT19937_stream_cipher_and_break_it.py
+++ Set3_Block_and_Stream_Crypto/c24_create_the_MT19937_stream_cipher_and_break_it.py
@@ -11,3 +11,74 @@ Use the same idea to generate a random "password reset token" using MT19937 seed
 
 Write a function to check if any given password token is actually the product of an MT19937 PRNG seeded with the current time.
 """
+
+import random
+import time
+import os
+
+
+def mt19937_stream_cipher(data: bytes, seed: int) -> bytes:
+    rng = random.Random(seed)
+    result = bytearray()
+    for b in data:
+        keystream_byte = rng.getrandbits(8)
+        result.append(b ^ keystream_byte)
+    return bytes(result)
+
+def brute_force_seed(ciphertext: bytes, known_plaintext: bytes) -> int:
+    print(f"[*]Cracking 16-bit seed...")
+    for seed in range(65536):
+        decrypted = mt19937_stream_cipher(ciphertext, seed)
+        if decrypted.endswith(known_plaintext):
+            return seed
+    return None
+
+def generate_time_token():
+    seed = int(time.time())
+    rng = random.Random(seed)
+    return rng.randbytes(16).hex(), seed
+
+def check_if_time_seeded(token_hex: str, window = 3000):
+    token_bytes = bytes.fromhex(token_hex)
+    now = int(time.time())
+
+    for t in range(now - window, now + 1):
+        rng = random.Random(t)
+        if rng.randbytes(16) == token_bytes:
+            return True, t
+    return False, None
+
+if __name__ == "__main__":
+    print("---Task A: 16-bits seed enc&break---")
+    # construct: random prefix + 14 'A'
+    prefix = os.urandom(random.randint(5, 15))
+    known_part = b'A' * 14
+    plaintext = prefix + known_part
+
+    true_seed = random.randint(0, 65535)
+    ciphertext = mt19937_stream_cipher(plaintext, true_seed)
+
+    print(f"[+]Original plaintext: {plaintext}")
+    print(f"[+]Original seed: {true_seed}")
+
+    found_seed = brute_force_seed(ciphertext, known_part)
+    print(f"[+]Crack seed: {found_seed}")
+
+    # validate dec result
+    if found_seed == true_seed:
+        print("[+]Decrypted validation success!")
+
+    print("\n" + "-" * 40 + "\n")
+
+    print("---Task B: Timestamp Token Generation and Detection---")
+    token, actual_time = generate_time_token()
+    print(f"[+]Generated tokens: {token}")
+    print(f"[+]Generated timestamp: {actual_time}")
+
+    # simulate attacker detection
+    is_weak, leaked_seed = check_if_time_seeded(token)
+
+    if is_weak:
+        print(f"[!]Warning: Weak seed token detected! Seed (timestamp) is: {leaked_seed}")
+    else:
+        print(f"[-]No time-based seeds detected.")
